f50a81e204ef1d3c04c420eb78208ed998f53b32
cleonst427 <cleons@pucp.edu.pe>
2021-08-24 10:57:28 -0500

    Integración de BBDD

--- wp-config.php
+++ wp-config.php
@@ -23,13 +23,13 @@
 define( 'DB_NAME', 'db_terciopelo' );
 
 /** MySQL database username */
-define( 'DB_USER', 'root' );
+define( 'DB_USER', 'admin');
 
 /** MySQL database password */
-define( 'DB_PASSWORD', 'root' );
+define( 'DB_PASSWORD', 'inovaweb:us-central1:preprod-heroku' );
 
 /** MySQL hostname */
-define( 'DB_HOST', 'localhost' );
+define( 'DB_HOST', 'inovaweb:us-central1:preprod-heroku' );
 
 /** Database Charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8mb4' );
